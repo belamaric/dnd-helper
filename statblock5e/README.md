1c5da6c4fd962a8697688546f997b3291718e148
John Belamaric <jbelamaric@infoblox.com>
2017-03-06 11:49:08 -0500

    Basic stat block formatter

---
+++ statblock5e/README.md
@@ -0,0 +1,3 @@
+# D&D Pretty Print
+
+`dndpp` is a utility to convert Lion's Den XML files into D&D 5e stat blocks.
---
+++ statblock5e/types.go
@@ -0,0 +1,221 @@
+package main
+
+import (
+	"io"
+	"encoding/xml"
+	"strings"
+	"text/template"
+)
+
+type Compendium struct {
+	XMLName xml.Name `xml:"compendium"`
+	Monsters []Monster `xml:"monster"`
+}
+
+type Trait struct {
+	XMLName xml.Name
+	Name string `xml:"name"`
+	Text []string `xml:"text"`
+	Attack []string `xml:"attack"`
+}
+
+func (t Trait) FormattedText() []string {
+	f := make([]string, len(t.Text))
+	for i, s := range t.Text {
+		f[i] = strings.Replace(s, "Hit:", "<i>Hit:</i>", -1)
+	}
+	return f
+}
+
+type Monster struct {
+	XMLName xml.Name `xml:"monster"`
+	Name string `xml:"name"`
+	Size string `xml:"size"`
+	Type string `xml:"type"`
+	Alignment string `xml:"alignment"`
+	Ac string `xml:"ac"`
+	Hp string `xml:"hp"`
+	Speed string `xml:"speed"`
+	Str string `xml:"str"`
+	Dex string `xml:"dex"`
+	Con string `xml:"con"`
+	Int string `xml:"int"`
+	Wis string `xml:"wis"`
+	Cha string `xml:"cha"`
+	Senses string `xml:"senses"`
+	Skill string `xml:"skill"`
+	Save string `xml:"save"`
+	DamageImmunity string `xml:"immune"`
+	ConditionImmunity string `xml:"conditionImmune"`
+	Vulnerabilities string `xml:"vulnerable"`
+	Resistances string `xml:"resist"`
+	Passive string `xml:"passive"`
+	Languages string `xml:"languages"`
+	Cr string `xml:"cr"`
+
+	Traits []Trait `xml:"trait"`
+	Actions []Trait `xml:"action"`
+	Reactions []Trait `xml:"reaction"`
+	Legendary []Trait `xml:"legendary"`
+       	Extras []struct {
+       	     XMLName xml.Name
+       	     Content string `xml:",innerxml"`
+        } `xml:",any"`
+}
+
+func (m Monster) SizeName() (string) {
+	switch m.Size {
+	case "M":
+		return "medium"
+	case "L":
+		return "large"
+	case "S":
+		return "small"
+	case "T":
+		return "tiny"
+	default:
+		return m.Size
+	}
+}
+
+func (m Monster) Subtitle() (string) {
+	return m.SizeName() + ", " + m.Type + ", " + m.Alignment
+}
+
+func (m Monster) EncodeStatBlock(w io.Writer) error {
+	tmplTxt := `
+<stat-block>
+ <creature-heading>
+  <h1>{{.Name}}</h1>
+  <h2>{{.Subtitle}}</h2>
+ </creature-heading>
+ <top-stats>
+  <property-line>
+   <h4>Armor Class</h4>
+   <p>{{.Ac}}</p>
+  </property-line>
+  <property-line>
+   <h4>Hit Points</h4>
+   <p>{{.Hp}}</p>
+  </property-line>
+  <property-line>
+   <h4>Speed</h4>
+   <p>{{.Speed}}</p>
+  </property-line>
+  <abilities-block data-str="{{.Str}}"
+                   data-dex="{{.Dex}}"
+                   data-con="{{.Con}}"
+                   data-int="{{.Int}}"
+                   data-wis="{{.Wis}}"
+                   data-cha="{{.Cha}}"></abilities-block>
+
+{{with .DamageImmunity}}
+  <property-line>
+   <h4>Damage Immunities</h4>
+   <p>{{.}}</p>
+  </property-line>
+{{end}}
+{{with .ConditionImmunity}}
+  <property-line>
+   <h4>Condition Immunities</h4>
+   <p>{{.}}</p>
+  </property-line>
+{{end}}
+{{with .Vulnerabilities}}
+  <property-line>
+   <h4>Vulnerabilities</h4>
+   <p>{{.}}</p>
+  </property-line>
+{{end}}
+{{with .Resistances}}
+  <property-line>
+   <h4>Resistances</h4>
+   <p>{{.}}</p>
+  </property-line>
+{{end}}
+{{with .Save}}
+  <property-line>
+   <h4>Saving Throws</h4>
+   <p>{{.}}</p>
+  </property-line>
+{{end}}
+{{with .Skill}}
+  <property-line>
+   <h4>Skills</h4>
+   <p>{{.}}</p>
+  </property-line>
+{{end}}
+{{with .Senses}}
+  <property-line>
+   <h4>Senses</h4>
+   <p>{{.}}</p>
+  </property-line>
+{{end}}
+  <property-line>
+   <h4>Passive Perception</h4>
+   <p>{{.Passive}}</p>
+  </property-line>
+{{with .Languages}}
+  <property-line>
+   <h4>Languages</h4>
+   <p>{{.}}</p>
+  </property-line>
+{{end}}
+  <property-line>
+   <h4>Challenge</h4>
+   <p>{{.Cr}}</p>
+  </property-line>
+ </top-stats>
+
+ {{range .Traits}}
+ <property-block>
+  <h4>{{.Name}}.</h4>
+  {{range .FormattedText}}
+  <p>{{.}}</p>
+  {{end}}
+ </property-block>
+ {{end}}
+
+ {{if len .Actions}}
+  <h3>Actions</h3>
+ {{range .Actions}}
+  <property-block>
+    <h4>{{.Name}}.</h4>
+  {{range .FormattedText}}
+  <p>{{.}}</p>
+  {{end}}
+  </property-block>
+ {{end}}
+ {{end}}
+
+ {{if len .Reactions}}
+  <h3>Reactions</h3>
+ {{range .Reactions}}
+  <property-block>
+    <h4>{{.Name}}.</h4>
+  {{range .FormattedText}}
+  <p>{{.}}</p>
+  {{end}}
+  </property-block>
+ {{end}}
+ {{end}}
+
+ {{if len .Legendary}}
+  <h3>Legendary Actions</h3>
+ {{range .Legendary}}
+  <property-block>
+    <h4>{{.Name}}.</h4>
+  {{range .FormattedText}}
+  <p>{{.}}</p>
+  {{end}}
+  </property-block>
+ {{end}}
+ {{end}}
+</stat-block>
+`
+	tmpl, err := template.New("stat-block").Parse(tmplTxt)
+	if err != nil { return err }
+	err = tmpl.Execute(w, m)
+	if err != nil { return err }
+	return nil
+}
